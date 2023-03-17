8bcd9e1f4928c15e0debe46a96e7c7cbc6137757
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-03-17 21:05:48 +0100

    Wrote tests for the PostfixToInfix function

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ module github.com/roman-mazur/architecture-lab-2
 go 1.20
 
 // TODO: Змініть, якщо вам потрібно використовувати іншу бібліотеку.
-require github.com/stretchr/testify v1.7.0
+
+require gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- implementation_test.go
+++ implementation_test.go
@@ -3,21 +3,58 @@ package lab2
 import (
 	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
+	 . "gopkg.in/check.v1"
 )
 
-func TestPostfixToInfix(t *testing.T) {
-	res, err := PostfixToInfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
-}
+type PostfixToInfixSuite struct{}
 
-func ExamplePrefixToPostfix() {
-	res, _ := PostfixToInfix("+ 2 2")
+var _ = Suite(&PostfixToInfixSuite{})
+
+func (s *PostfixToInfixSuite) TestPostfixToInfix(c *C) {
+	// звичайний вираз
+	res, err := PostfixToInfix("2 3 +")
+	c.Assert(err, IsNil)
+	c.Assert(res, Equals, "2 + 3")
+
+	// вираз з різними операціями
+	res, err = PostfixToInfix("5 1 2 + 4 * + 3 -")
+	c.Assert(err, IsNil)
+	c.Assert(res, Equals, "5 + (1 + 2) * 4 - 3")
+
+	// некоректні дані
+	res, err = PostfixToInfix("")
+	c.Assert(err, NotNil)
+
+	res, err = PostfixToInfix("a b +")
+	c.Assert(err, NotNil)
+
+	// тест з повідомлення про помилку
+	res, err = PostfixToInfix("4 2 - 3 * 5 +")
+	c.Assert(err, IsNil)
+	c.Assert(res, Equals, "(4 - 2) * 3 + 5")
+
+	// недостатньо оперторів
+	res, err = PostfixToInfix("2 3 + 4 5 -")
+	c.Assert(err, NotNil);
+
+	// некоректний символ
+	res, err = PostfixToInfix("2 3 / 4 5 & -")
+	c.Assert(err, NotNil);
+
+	//більш складний вираз
+	res, err = PostfixToInfix("1 2 + 3 4 - 5 * ^ 6 7 / /")
+	c.Assert(err, IsNil)
+	c.Assert(res, Equals, "((1 + 2) ^ ((3 - 4) * 5)) / (6 / 7)")
+	
+}
+func Test(t *testing.T) { TestingT(t) }
+
+
+
+func ExamplePostfixToInfix() {
+	res, _ := PostfixToInfix("8 3 + 9 -")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 8 + 3 - 9
 }
